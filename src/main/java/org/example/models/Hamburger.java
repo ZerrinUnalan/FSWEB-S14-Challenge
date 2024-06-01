b0120ac56be36d8ac1d14e82846bb01ebc7d9959
Zerrin Unalan <zerunalan@gmail.com>
2024-06-01 17:06:41 +0300

    Tested

---
+++ src/main/java/org/example/models/Hamburger.java
@@ -0,0 +1,108 @@
+package org.example.models;
+
+public class Hamburger {
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+    private String addition1Name;
+    private double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private double addition3Price;
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+
+    public void addHamburgerAddition1(String name, double price) {
+        this.addition1Name = name;
+        this.addition1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        this.addition3Name = name;
+        this.addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        this.addition4Name = name;
+        this.addition4Price = price;
+    }
+
+    public double itemizeHamburger() {
+        double totalPrice = this.price;
+        if (this.addition1Name != null) {
+            totalPrice += this.addition1Price;
+        }
+        if (this.addition2Name != null) {
+            totalPrice += this.addition2Price;
+        }
+        if (this.addition3Name != null) {
+            totalPrice += this.addition3Price;
+        }
+        if (this.addition4Name != null) {
+            totalPrice += this.addition4Price;
+        }
+        return totalPrice;
+    }
+
+    public double getPrice() {
+        return itemizeHamburger();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public String getBreadRollType() {
+        return breadRollType;
+    }
+
+    public String getAddition1Name() {
+        return addition1Name;
+    }
+
+    public double getAddition1Price() {
+        return addition1Price;
+    }
+
+    public String getAddition2Name() {
+        return addition2Name;
+    }
+
+    public double getAddition2Price() {
+        return addition2Price;
+    }
+
+    public String getAddition3Name() {
+        return addition3Name;
+    }
+
+    public double getAddition3Price() {
+        return addition3Price;
+    }
+
+    public String getAddition4Name() {
+        return addition4Name;
+    }
+
+    public double getAddition4Price() {
+        return addition4Price;
+    }
+}
